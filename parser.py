f77a7320ed1a08682123328b6f28d3a3d03a9d53
EnesBaytekin <enesbaytekin38@gmail.com>
2024-11-01 02:20:16 +0300

    chore: add statement to parser

--- parser.py
+++ parser.py
@@ -43,5 +43,49 @@ class Parser:
         self.index = checkpoint
         return Node("statements")
     def parse_statement(self):
-        
+        checkpoint = self.index
+        if self.match("{") is not None:
+            statements = self.parse_statements()
+            if statements is not None:
+                if self.match("}") is not None:
+                    return Node("statement", statements)
+        self.index = checkpoint
+        expression = self.parse_expression()
+        if expression is not None:
+            return Node("statement", expression)
+        self.index = checkpoint
+        assignment = self.parse_assignment()
+        if assignment is not None:
+            return Node("statement", assignment)
+        self.index = checkpoint
+        if self.match("DEL") is not None:
+            VAR = self.match("VAR")
+            if VAR is not None:
+                if self.match("[") is not None:
+                    expression = self.parse_expression()
+                    if expression is not None:
+                        if self.match("]") is not None:
+                            return Node("statement", VAR, expression)
+        self.index = checkpoint
+        function_definition = self.parse_function_definition()
+        if function_definition is not None:
+            return Node("statement", function_definition)
+        self.index = checkpoint
+        if_statement = self.parse_if_statement()
+        if if_statement is not None:
+            return Node("statement", if_statement)
+        self.index = checkpoint
+        for_loop = self.parse_for_loop()
+        if for_loop is not None:
+            return Node("statement", for_loop)
+        self.index = checkpoint
+        while_loop = self.parse_while_loop()
+        if while_loop is not None:
+            return Node("statement", while_loop)
+    def parse_expression(self):
+    def parse_assignment(self):
+    def parse_function_definition(self):
+    def parse_if_statement(self):
+    def parse_for_loop(self):
+    def parse_while_loop(self):
         
